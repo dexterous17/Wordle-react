d4ff768360999a16250e6be1b9f40fe8b8f4f39b
IamDexterous <harshilprajapatiofficial@gmail.com>
2022-05-07 01:51:19 -0300

    Used Json server to find the word from json list

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [solution,setSolution] = useState(null)
-  useEffect(()=>{
 
-  },[])
+  const [solution, setSolution] = useState()
+
+  useEffect(() => {
+    fetch('http://localhost:3000/solutions')
+      .then(res => res.json())
+      .then(json => {
+        const randomSolution = json[Math.floor(Math.random() * json.length)]
+        setSolution(randomSolution.word)
+      })
+
+  }, [setSolution])
 
   return (
     <div className="App">
-      React Wordle
+      <h1>React Wordle</h1>
+      {solution && <div>Solution : {solution}</div>}
     </div>
   );
 }
